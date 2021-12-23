28070a8a2fbbbe2e4ab8b3ca9e72d4d2325dd633
kisgamer <64792441+kisgamer@users.noreply.github.com>
2021-12-23 16:56:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
 	$uri = 'http://';
 }
 $uri .= $_SERVER['HTTP_HOST'];
-header('Location: '.$uri.'/index.html');
+header('Location: '.$uri.'/home.html');
 ?>
